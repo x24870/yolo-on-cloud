1f74face2bdf515354e2ad5fb23ba35e203f6581
x24870 <x24870@gmail.com>
2021-08-30 22:04:40 +0800

    add color code for different classes and show confidence scores

--- webserver/public/static/scripts/draw_functions.js
+++ webserver/public/static/scripts/draw_functions.js
@@ -2,35 +2,44 @@ function intervalWebcamFrame (){
     // This draws the video then updates the bounding boxes. First offscreen rendering.
     ctxWcVideo.drawImage(video, 0, 0, wc_coords[2], wc_coords[3]);
     currentData = JSON.parse(tempData);
-      var PADDING = 0;
-      var x,y,w,h;
-        // Draw the bounding boxes
-
+    var PADDING = 0;
+    var x,y,w,h;
     var data = currentData;
+
+    // update color code for classes
+    updateDrawingTable(data);
+
+    // Draw the bounding boxes
     if (data.type == "detection_data"){
         for (var j = 0, len2 = data.bbs.length; j < len2; ++j ){
             var bb = data.bbs[j];
             var text = data.classes[j];
-            // Draw the rectangle that will contain the object name.
-            ctxWcVideo.fillStyle="#000000";
+            // Draw the rectangle that will contain the object name and confidence socre.
+            ctxWcVideo.fillStyle = classesTable[text].color;
             x = bb[1];
             // Ensure that the rectangle is inside the allowable area.
             y = bb[0] + 0;
             w = bb[3] - bb[1];
-            //h = TEXT_BOX_HEIGHT;
             h = Math.ceil(y*TEXT_BOX_HEIGHT);
             ctxWcVideo.fillRect(x,y,w,h);
+
+            // Draw the bounding box around the given obect
+            ctxWcVideo.strokeStyle = classesTable[text].color;
+            ctxWcVideo.strokeRect(bb[1],
+              bb[0],
+              bb[3] - bb[1] - PADDING,
+              bb[2] - bb[0] - PADDING);
+
             // Draw the text inside the rectangle
             ctxWcVideo.font = bbTextSize + "pt Arial";
             ctxWcVideo.fillStyle = '#FFFFFF'
             ctxWcVideo.fillText(text,
                          x + 0, 
                          y + bbTextHPadding);
-            // Draw the bounding box around the given obect
-            ctxWcVideo.strokeRect(bb[1],
-                           bb[0],
-                           bb[3] - bb[1] - PADDING,
-                           bb[2] - bb[0] - PADDING);
+            let score = data.scores[j].toFixed(2)
+            ctxWcVideo.fillText(score,
+                          x + 0, 
+                          y + bbTextHPadding*2);
         }
     }
         
@@ -39,8 +48,23 @@ function intervalWebcamFrame (){
 };
     
 
+function updateDrawingTable(data) {
+  for(let i=0; i<data.classes.length; i++){
+    let className = data.classes[i]
 
+    // generate hex color code for class
+    if( !classesTable.hasOwnProperty(className) ){
+      // generate color hex code for this class
+      let color = '#'+(Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0');
+      classesTable[className] = {}
+      classesTable[className]['color'] = color;
+    }
 
+    // update bounding box
+    classesTable[className]['bbs'] = data.bbs[i];
+    classesTable[className]['scores'] = data.scores[i];
+  }
+}
 
 
 
--- webserver/public/static/scripts/setup_rtc.js
+++ webserver/public/static/scripts/setup_rtc.js
@@ -79,13 +79,13 @@ function startDataChannels() {
     };
     dc.onmessage = function(evt) {
       // Receive Data
-      tempData = evt.data;
+      if(!DEBUG) {tempData = evt.data;}
       // Send the data.
-      dc.send(JSON.stringify({'image_properties':{'height':wc_coords[3],
-                                         'width':wc_coords[2],
-                                        }
-                              }));
-        console.log('***message received: ' + tempData)
+      dc.send(JSON.stringify({'image_properties':{
+                                'height':wc_coords[3],
+                                'width':wc_coords[2],
+                                }}));
+      console.log('*** message received: ' + tempData)
         
     };
 
